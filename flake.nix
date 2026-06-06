6c082e710332bcdd0bb29d62af5daaed7be66237
ZPeppler <peppler.zachary@gmail.com>
2026-06-06 14:11:27 -0400

    Initial flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A complete workflow of tmux, sesh, and nixvim to replace vscode.";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixvim,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = nixvim.legacyPackages.${system}.makeNixvim {
+        plugins = {
+          lualine.enable = true;
+          bufferline.enable = true;
+          neo-tree.enable = true;
+        };
+      };
+    };
+}
+
