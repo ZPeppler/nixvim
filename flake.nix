7c1b2cae93e3ca658758b2b63aa30f366235877d
ZPeppler <peppler.zachary@gmail.com>
2026-06-06 15:43:30 -0400

    Changed flake setup

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "A complete workflow of tmux, sesh, and nixvim to replace vscode.";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,13 +13,21 @@
       self,
       nixpkgs,
       nixvim,
+      home-manager,
     }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      hmConfig = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./config.nix
+          nixvim.homeManagerModules.nixvim
+        ];
+      };
     in
     {
-      packages.${system}.default = nixvim.legacyPackages.${system}.makeNixvim (import ./config.nix);
+      packages.${system}.default = hmConfig.config.programs.nixvim.package;
     };
 }
 
