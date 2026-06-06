aae91385b2cca327c778ce3a8b27b221371542de
ZPeppler <peppler.zachary@gmail.com>
2026-06-06 15:34:27 -0400

    Created better structure

--- config.nix
+++ config.nix
@@ -1,20 +1,7 @@
 { pkgs, ... }:
 {
   imports = [
-    ./settings
+    ./nixvim
   ];
-  colorschemes.tokyonight = {
-    enable = true;
-    settings = {
-      style = "storm";
-    };
-  };
-  colorscheme = "tokyonight";
-  plugins = {
-    web-devicons.enable = true;
-    lualine.enable = true;
-    bufferline.enable = true;
-    neo-tree.enable = true;
-  };
 }
 
---
+++ nixvim/default.nix
@@ -0,0 +1,20 @@
+{
+  imports = [
+    ./editor
+    ./settings
+  ];
+  colorschemes.tokyonight = {
+    enable = true;
+    settings = {
+      style = "storm";
+    };
+  };
+  colorscheme = "tokyonight";
+  plugins = {
+    web-devicons.enable = true;
+    lualine.enable = true;
+    bufferline.enable = true;
+    neo-tree.enable = true;
+  };
+}
+
