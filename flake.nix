c56223c9e652af8d47867601c95ef8e280b320ec
ZPeppler <peppler.zachary@gmail.com>
2026-06-07 08:12:42 -0400

    Created standalone flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A complete workflow of tmux, sesh, and nixvim to replace vscode.";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    # home-manager.url = "github:nix-community/home-manager";
     nixvim.url = "github:nix-community/nixvim";
   };
   outputs =
@@ -10,18 +10,28 @@
       self,
       nixpkgs,
       nixvim,
-      home-manager,
+      # home-manager,
     }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
     {
-      homeConfigurations."zpeppler" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
-        modules = [
-          nixvim.homeModules.nixvim
-          ./config.nix
-        ];
+      packages.${system}.default = nixvim.legacyPackages.${system}.makeNixvim {
+        pkgs = pkgs;
+        module = import ./config.nix;
       };
     };
+  # {
+  #   homeConfigurations."zpeppler" = home-manager.lib.homeManagerConfiguration {
+  #     pkgs = import nixpkgs {
+  #       system = "x86_64-linux";
+  #     };
+  #     modules = [
+  #       nixvim.homeModules.nixvim
+  #       ./config.nix
+  #     ];
+  #   };
+  # };
 }
 
