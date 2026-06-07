8fbf39679cde0620812cc65f55f3250ee7201695
ZPeppler <peppler.zachary@gmail.com>
2026-06-07 11:12:54 -0400

    Removed home-manager stuff

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "A complete workflow of tmux, sesh, and nixvim to replace vscode.";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager.url = "github:nix-community/home-manager";
     nixvim.url = "github:nix-community/nixvim";
   };
   outputs =
@@ -10,7 +9,6 @@
       self,
       nixpkgs,
       nixvim,
-      # home-manager,
     }:
     let
       system = "x86_64-linux";
@@ -21,16 +19,5 @@
         import ./config.nix { inherit pkgs; }
       );
     };
-  # {
-  #   homeConfigurations."zpeppler" = home-manager.lib.homeManagerConfiguration {
-  #     pkgs = import nixpkgs {
-  #       system = "x86_64-linux";
-  #     };
-  #     modules = [
-  #       nixvim.homeModules.nixvim
-  #       ./config.nix
-  #     ];
-  #   };
-  # };
 }
 
